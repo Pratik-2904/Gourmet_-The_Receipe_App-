efce806f382a876f3ae97327b27bb23bdf77165d
Pratik <pratiksukale2904@gmail.com>
2024-05-27 00:52:46 +0530

    Addition of main viewmoel and Ui with required permissions

--- app/src/main/java/com/example/gourmet_therecepieapp/ApiService.kt
+++ app/src/main/java/com/example/gourmet_therecepieapp/ApiService.kt
@@ -1,9 +1,21 @@
 package com.example.gourmet_therecepieapp
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+
+//builds connection to url and convert the incoming data  to data class compatible data
+//builder used to
+private val retrofit = Retrofit.Builder().baseUrl("https://www.themealdb.com/api/json/v1/1")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+    val receipeService = retrofit.create(ApiService::class.java)
+
 
 interface ApiService{
     //gets http request to categories.php
     @GET("categories.php")
     suspend fun getCategories():CategoryResponse
+
 }
